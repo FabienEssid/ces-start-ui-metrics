662050c1f3f5602fbf67ad13d9b83031c70ba8f3
Fabien Essid <fabien.essid@bearstudio.fr>
2022-11-11 12:57:00 +0100

    feat: add twitter feed

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -1,14 +1,23 @@
 import Hero from "@ulixee/hero-playground";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const getSocialMediaStatistics = async () => {
+const getGitHubStartUiStats = async () => {
     const hero = new Hero();
-
     await hero.goto("https://github.com/BearStudio/start-ui-web");
     const startUiStarsCounter = await hero.document.querySelector(".Counter.js-social-count").innerText;
 
+    return startUiStarsCounter;
+};
+
+const getGitHubStartUiNativeStats = async () => {
+    const hero = new Hero();
     await hero.goto("https://github.com/BearStudio/start-ui-native");
     const startUiNativeStarsCounter = await hero.document.querySelector(".Counter.js-social-count").innerText;
+
+    return startUiNativeStarsCounter;
+};
+const getTwitterStartUiStats = async () => {
+    const hero = new Hero();
 
     await hero.goto("https://foller.me/startui_");
     const startUiFollowers = await hero.document
@@ -16,12 +25,32 @@ const getSocialMediaStatistics = async () => {
         .querySelectorAll(".col-12.mb025")[0]
         .querySelector(".right").textContent;
 
-    await hero.close();
+    return startUiFollowers;
+};
+const getTwitterBearstudioStats = async () => {
+    const hero = new Hero();
+    await hero.goto("https://foller.me/_bearstudio");
+    const bearstudioFollowers = await hero.document
+        .querySelector(".md-col.md-col-6.col-12.md-pl3")
+        .querySelectorAll(".col-12.mb025")[0]
+        .querySelector(".right").textContent;
 
-    return { startUi: startUiStarsCounter, startUiNative: startUiNativeStarsCounter, startUiFollowers };
+    return bearstudioFollowers;
 };
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse<unknown>) {
-    const statistics = await getSocialMediaStatistics();
-    return response.status(200).json(statistics);
+    const [gitHubStartUiStars, gitHubStartUiNativeStars, twitterStartUiFollowers, twitterBearStudioFollowers] =
+        await Promise.all([
+            getGitHubStartUiStats(),
+            getGitHubStartUiNativeStats(),
+            getTwitterStartUiStats(),
+            getTwitterBearstudioStats(),
+        ]);
+
+    return response.status(200).json({
+        startUi: gitHubStartUiStars,
+        startUiNative: gitHubStartUiNativeStars,
+        startUiFollowers: twitterStartUiFollowers,
+        bearstudioFollowers: twitterBearStudioFollowers,
+    });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,76 @@
 import { Box, Stat, StatLabel, StatNumber, StatHelpText, Text, VStack, Flex } from "@chakra-ui/react";
 import { useGetGitHubStats } from "../services/data";
 import { FaGithub, FaTwitter } from "react-icons/fa";
+import { Timeline } from "react-twitter-widgets";
 
 export default function Home() {
-    const { data, isFetching } = useGetGitHubStats({
-        // refetchInterval: 30000,
+    const { data, isFetching, isError } = useGetGitHubStats({
+        refetchInterval: 60000,
     });
-    console.log({ data });
     const { data: stats } = data || { data: null };
-    const { startUi, startUiNative, startUiFollowers } = stats || {
+    const { startUi, startUiNative, startUiFollowers, bearstudioFollowers } = stats || {
         startUi: "N/A",
         startUiNative: "N/A",
         startUiFollowers: "N/A",
+        bearstudioFollowers: "N/A",
     };
 
     return (
-        <Box>
-            <VStack spacing="4" width="full" padding="4">
-                <Stat boxShadow="md" borderRadius="md" padding="4" w="full" h="full">
-                    <StatLabel fontSize="3xl">
-                        <Flex alignItems="center">
-                            <FaGithub />
-                            <Text pl="2">Start UI - GitHub</Text>
-                        </Flex>
-                    </StatLabel>
-                    <StatNumber fontSize="8xl">{startUi}</StatNumber>
-                    <StatHelpText>{isFetching ? "Loading..." : ""}</StatHelpText>
-                </Stat>
-                <Stat boxShadow="md" borderRadius="md" padding="4" w="full" h="full">
-                    <StatLabel fontSize="3xl">
-                        <Flex alignItems="center">
-                            <FaGithub />
-                            <Text pl="2">Start UI Native - GitHub</Text>
-                        </Flex>
-                    </StatLabel>
-                    <StatNumber fontSize="8xl">{startUiNative}</StatNumber>
-                    <StatHelpText>{isFetching ? "Loading..." : ""}</StatHelpText>
-                </Stat>
-
-                <Stat boxShadow="md" borderRadius="md" padding="4" w="full" h="full">
-                    <StatLabel fontSize="3xl">
-                        <Flex alignItems="center">
-                            <FaTwitter />
-                            <Text pl="2">Start UI Twitter followers</Text>
-                        </Flex>
-                    </StatLabel>
-                    <StatNumber fontSize="8xl">{startUiFollowers}</StatNumber>
-                    <StatHelpText>{isFetching ? "Loading..." : ""}</StatHelpText>
-                </Stat>
-            </VStack>
+        <Box h="100vh">
+            {isError && <Text>An error occurred</Text>}
+            <Flex>
+                <VStack spacing="4" flex="2" padding="4">
+                    <Stat boxShadow="md" borderRadius="md" padding="4" w="full">
+                        <StatLabel fontSize="3xl">
+                            <Flex alignItems="center">
+                                <FaGithub />
+                                <Text pl="2">Start UI - GitHub</Text>
+                            </Flex>
+                        </StatLabel>
+                        <StatNumber fontSize="8xl">{startUi}</StatNumber>
+                        <StatHelpText>{isFetching ? "Refreshing..." : ""}</StatHelpText>
+                    </Stat>
+                    <Stat boxShadow="md" borderRadius="md" padding="4" w="full">
+                        <StatLabel fontSize="3xl">
+                            <Flex alignItems="center">
+                                <FaGithub />
+                                <Text pl="2">Start UI Native - GitHub</Text>
+                            </Flex>
+                        </StatLabel>
+                        <StatNumber fontSize="8xl">{startUiNative}</StatNumber>
+                        <StatHelpText>{isFetching ? "Refreshing..." : ""}</StatHelpText>
+                    </Stat>
+                    <Stat boxShadow="md" borderRadius="md" padding="4" w="full">
+                        <StatLabel fontSize="3xl">
+                            <Flex alignItems="center">
+                                <FaTwitter />
+                                <Text pl="2">Start UI Twitter followers</Text>
+                            </Flex>
+                        </StatLabel>
+                        <StatNumber fontSize="8xl">{startUiFollowers}</StatNumber>
+                        <StatHelpText>{isFetching ? "Refreshing..." : ""}</StatHelpText>
+                    </Stat>
+                    <Stat boxShadow="md" borderRadius="md" padding="4" w="full">
+                        <StatLabel fontSize="3xl">
+                            <Flex alignItems="center">
+                                <FaTwitter />
+                                <Text pl="2">BearStudio followers</Text>
+                            </Flex>
+                        </StatLabel>
+                        <StatNumber fontSize="8xl">{bearstudioFollowers}</StatNumber>
+                        <StatHelpText>{isFetching ? "Refreshing..." : ""}</StatHelpText>
+                    </Stat>
+                </VStack>
+                <Box p="4" flex="1" maxH="100vh">
+                    <Timeline
+                        dataSource={{
+                            sourceType: "url",
+                            url: "https://twitter.com/startui_?ref_src=twsrc%5Etfw",
+                        }}
+                        options={{ width: "auto" }}
+                    />
+                </Box>
+            </Flex>
         </Box>
     );
 }
